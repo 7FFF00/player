6bad7ee2879ec72cb6b7627b330567471a53f345
7FFF00 <t42please@gmail.com>
2020-01-23 16:47:24 -0800

    SETUP development environment

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const ENTRY_FILE = path.resolve(__dirname, 'client', 'src', 'app.jsx');
+const OUT_DIR = path.resolve(__dirname, 'client', 'dist');
+
+module.exports = {
+  mode: 'development',
+  entry: ENTRY_FILE,
+  output: {
+    path: OUT_DIR,
+    filename: 'player_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};

f4cf3c142c8073cb971ba960a33bfacdb81d2712
7FFF00 <t42please@gmail.com>
2020-01-27 12:03:19 -0800

    SETUP testing suite of jest and enzyme

---
+++ client/src/components/App.spec.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow, mount, render, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from './App';
+
+configure({ adapter: new Adapter() });
+
+describe('Test Suite', () => {
+  it('should render without throwing an error', () => {
+    const app = shallow(<App />);
+    expect(app.text()).toEqual('App');
+  });
+});

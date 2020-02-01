a34fd154736e59bcbbe50c9ae3c79704107a7605
7FFF00 <t42please@gmail.com>
2020-01-31 17:51:50 -0800

    IMPLEMENT basic structure for playInfo display component

--- client/src/components/PlayInfo/PlayInfo.jsx
+++ client/src/components/PlayInfo/PlayInfo.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TimeDisplay from './TimeDisplay';
 import Waveform from './Waveform';
+import Comments from './Comments';
+import { RelativeDiv, OverlappingDiv, CommentsContainer } from './playInfoStyles';
 
-const PlayInfo = (props) => (<div>PlayInfo</div>);
+const PlayInfo = ({
+  waveform,
+  playTime,
+  duration,
+  comments,
+}) => (
+  <RelativeDiv>
+    <OverlappingDiv>
+      <Waveform waveform={waveform} />
+    </OverlappingDiv>
+    <OverlappingDiv>
+      <TimeDisplay playTime={playTime} duration={duration} />
+    </OverlappingDiv>
+    <CommentsContainer>
+      <Comments comments={comments} />
+    </CommentsContainer>
+  </RelativeDiv>
+);
+
+PlayInfo.propTypes = {
+  waveform: PropTypes.arrayOf(PropTypes.number),
+  playTime: PropTypes.number,
+  duration: PropTypes.number,
+  comments: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
+};
+
+PlayInfo.defaultProps = {
+  waveform: [],
+  playTime: 0,
+  duration: 0,
+  comments: [],
+};
 
 export default PlayInfo;
---
+++ client/src/components/PlayInfo/playInfoStyles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const RelativeDiv = styled.div`
+  position: relative;
+  width: 95%;
+  margin: 0px 0px 0px 15px;
+  height: 180px;
+  border: solid black 1px;
+`;
+
+export const OverlappingDiv = styled.div`
+  position: absolute;
+`;
+
+export const CommentsContainer = styled.div`
+  position: absolute;
+  bottom: 40%;
+`;

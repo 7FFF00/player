bcd6e60ccb60fe7c061fedb85b4ebaf9a40ec94f
7FFF00 <t42please@gmail.com>
2020-02-06 18:26:34 -0800

    HANDLE bundle file request

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const fs = require('fs');
 const db = require('../database/db.js');
 
 const app = express();
@@ -15,11 +16,19 @@ app.listen(port, () => {
   console.log('Server listening on port', port);
 });
 
-app.get('/', (req, res, next) => {
-  console.log(`${req.method} on ${req.url}, with params of ${req.params}`);
-  next();
+app.get('/player_bundle', (req, res) => {
+  fs.readFile(path.resolve(__dirname, '..', 'client', 'dist', 'player_bundle.js'), (err, data) => {
+    console.log('Attempting to find and provide bundle.js');
+    if (err) {
+      console.log('ERROR RETRIEVING BUNDLE:', err);
+      res.status(500).send('Failed to find')
+    } else {
+      console.log('BUNDLE RETRIEVED, SENDING');
+      res.status(200).send(data);
+    }
+  });
 });
 
-app.get('/api/songs/:songId', (req, res) => db.readOne({ id: req.params.songId })
+app.get('/api/player/songs/:songId', (req, res) => db.readOne({ id: req.params.songId })
   .then((songData) => res.status(200).send(songData))
   .catch((err) => res.status(400).send(err)));
